30b81d2807109b2d5483c938867e07b14aae91e2
alvarot2601 <alvarot2601@gmail.com>
2023-10-19 14:01:42 +0200

    elimino archivo container

--- src/components/Container.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const Container = ({className, backgroundColor}) => {
-    return (
-        <div className={`container ${className}`} style={{background:backgroundColor}}>
-            
-        </div>
-    );
-}
-export default Container;
